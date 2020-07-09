dde4c87645d325683ee64a4cce2c227855bbd65c
akerketolegen21 <akerketolegen.at@gmail.com>
2020-07-10 01:32:48 +0600

    first try on company db

---
+++ lib/company_revier.dart
@@ -0,0 +1,7 @@
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
